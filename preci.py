5982696d2f8bf020fdc8f5c0a0fe5a690ebf3148
Pranjal <pranjalrastogi1998@gmail.com>
2018-10-21 00:02:44 +0530

    displace json dumps

---
+++ preci.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+
+#f = json.loads(open('./ground_water_quality.json').read())
+#print(f)
+
+#print(data.head)
+
+class Data:
+    def __init__(self, data , desc=''):
+        self.data = data
+        self.desc = desc
+
+Datas = [
+    Data(pd.read_csv('./data.csv', sep='\t'), "Displacement per year for each country"),
+    #Data(pd.read_csv('./gr_water_quality.csv'), "Ground water quality of various places")
+]
